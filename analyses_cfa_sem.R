404d64b51f3b50a5c1825cdd8a69ae9f3e3a4580
Audrey Mazancieux <audrey.mazancieux@gmail.com>
2025-02-28 16:40:13 +0100

    Update CFA analyses and models: use FILM

--- analyses_cfa_sem.R
+++ analyses_cfa_sem.R
@@ -34,8 +34,9 @@ all_dataset_bias <- read.csv("./bias_all_studies.csv", header=TRUE, sep=";", dec
 # full general model
 cfa_full_general <- 'g_metacog_eff =~ Auditory + Auditory_study3 + Visual + Visual_study3 + Pain + Tactile + EM + SM + VP + EF'
 fit_full_general <- cfa(model = cfa_full_general, 
+                        estimator = 'MLR',
                         data = all_dataset_Mratio, 
-                        missing="pairwise")
+                        missing="FIML")
 
 summary(fit_full_general, fit.measures=TRUE, standardized=TRUE)
 interpret(fit_full_general)
@@ -49,8 +50,9 @@ lavaanPlot(model = fit_full_general, sig=.05,stars=c("regress","latent","covs"),
 cfa_2_factors <- 'g_perception =~ Auditory + Auditory_study3 + Visual + Visual_study3 + Pain + Tactile + VP
                   g_cognitive =~ EM + SM + EF'
 fit_2_factors <- cfa(model = cfa_2_factors, 
+                     estimator = 'MLR',
                      data=all_dataset_Mratio, 
-                     missing="pairwise")
+                     missing="FIML")
 
 summary(fit_2_factors, fit.measures=TRUE, standardized=TRUE)
 interpret(fit_2_factors)
@@ -62,13 +64,15 @@ lavaanPlot(model = fit_2_factors, sig=.05,stars=c("regress","latent","covs"),
 
 
 # 4-factor model
+# --> NOT IDENTIFIED
 cfa_4_factors <- 'g_visual =~ + Visual + Visual_study3 + VP
                   g_cognitive =~ EM + SM + EF
                   g_auditory =~  Auditory + Auditory_study3
                   g_somato =~ Pain + Tactile'
 fit_4_factors <- cfa(model = cfa_4_factors, 
+                     estimator = 'MLR',
                      data=all_dataset_Mratio, 
-                     missing="pairwise")
+                     missing="FIML")
 
 summary(fit_4_factors, fit.measures=TRUE, standardized=TRUE)
 interpret(fit_4_factors)
@@ -82,13 +86,14 @@ lavaanPlot(model = fit_4_factors, sig=.05,stars=c("regress","latent","covs"),
 
 
 # study model
-# --> OVERFITS THE DATA 
+# --> NOT IDENTIFIED
 cfa_3_studies <- 'study_1 =~ VP + EM + SM + EF
                   study_2 =~ Auditory + Visual + Pain + Tactile
                   study_3 =~ Visual_study3 + Auditory_study3'
 fit_3_studies <- cfa(model = cfa_3_studies, 
+                     estimator = 'MLR',
                      data=all_dataset_Mratio, 
-                     missing="pairwise")
+                     missing="FIML")
 
 summary(fit_3_studies, fit.measures=TRUE, standardized=TRUE)
 interpret(fit_3_studies)
@@ -100,16 +105,20 @@ lavaanPlot(model = fit_3_studies, sig=.05,stars=c("regress","latent","covs"),
 
 
 # AIC comparision
-aictab(cand.set = list(fit_full_general, fit_2_factors, fit_4_factors), modnames = c('full generality', '2 factors', '4 factors'))
+aictab(cand.set = list(fit_full_general, fit_2_factors), modnames = c('full generality', '2 factors'))
 
 
 ## Confirmatory factor analyses: Study 1 and 3 -----------------------------
 
+data1and3 <- all_dataset_Mratio %>% 
+  filter(EM != "" | VP != "" | SM != "" | EF != "" )
+
 # full general model
 cfa_full_1_3 <- 'g_metacog_eff =~ Auditory_study3 + Visual_study3 + EM + VP + SM + EF'
 fit_full_1_3 <- fit_full_1_3 <- cfa(model = cfa_full_1_3, 
-                                    data = all_dataset_Mratio, 
-                                    missing="pairwise")
+                                    estimator = 'MLR',
+                                    data = data1and3, 
+                                    missing="FIML")
 
 summary(fit_full_1_3, fit.measures=TRUE, standardized=TRUE)
 interpret(fit_full_1_3)
@@ -123,8 +132,9 @@ lavaanPlot(model = fit_full_1_3, sig=.05,stars=c("regress","latent","covs"),
 cfa_2_factors_1_3 <- 'g_perception =~ VP + Visual_study3 + Auditory_study3
                       g_cognitive =~ EM + SM + EF'
 fit_2_factors_1_3 <- cfa(model = cfa_2_factors_1_3, 
-                         data = all_dataset_Mratio, 
-                         missing="pairwise")
+                         estimator = 'MLR',
+                         data = data1and3, 
+                         missing="FIML")
     
 summary(fit_2_factors_1_3, fit.measures=TRUE, standardized=TRUE)
 interpret(fit_2_factors_1_3)
@@ -138,8 +148,9 @@ lavaanPlot(model = fit_2_factors_1_3, sig=.05,stars=c("regress","latent","covs")
 cfa_study_1_3 <- 'g_study_1 =~ EM + VP + SM + EF  
                   g_study_3 =~  Auditory_study3 + Visual_study3'
 fit_study_1_3 <- cfa(model = cfa_study_1_3, 
-                     data = all_dataset_Mratio, 
-                     missing="pairwise")
+                     estimator = 'MLR',
+                     data = data1and3, 
+                     missing="FIML")
 
 summary(fit_study_1_3, fit.measures=TRUE, standardized=TRUE)
 interpret(fit_study_1_3)
@@ -156,11 +167,15 @@ aictab(cand.set = list(fit_full_1_3, fit_2_factors_1_3, fit_study_1_3), modnames
 
 ## Confirmatory factor analyses: Study 2 and 3 -----------------------------
 
+data2and3 <- all_dataset_Mratio %>% 
+  filter(Auditory != "" | Visual != "" | Tactile != "" | Pain != "" )
+
 # full general model
 cfa_full_2_3 <- 'g_metacog_eff =~ Auditory_study3 + Visual_study3 + Auditory + Visual + Pain + Tactile'
-fit_full_2_3 <- cfa(model = cfa_full_2_3, 
-                    data = all_dataset_Mratio, 
-                    missing="pairwise")
+fit_full_2_3 <- cfa(model = cfa_full_2_3,
+                    estimator = 'MLR',
+                    data = data2and3, 
+                    missing="FIML")
 
 summary(fit_full_2_3, fit.measures=TRUE, standardized=TRUE)
 interpret(fit_full_2_3)
@@ -171,13 +186,13 @@ lavaanPlot(model = fit_full_2_3, sig=.05,stars=c("regress","latent","covs"),
            coefs = TRUE, covs = TRUE, stand=TRUE)
 
 # 3-factor model
-# --> OVERFITS THE DATA
 cfa_3_factors_2_3 <- 'g_visual =~ Visual + Visual_study3
                       g_auditory =~ Auditory + Auditory_study3
                       g_somato =~ Pain + Tactile'
 fit_3_factors_2_3 <- cfa(model = cfa_3_factors_2_3, 
-                         data = all_dataset_Mratio, 
-                         missing="pairwise")
+                         estimator = 'MLR',
+                         data = data2and3, 
+                         missing="FIML")
   
 summary(fit_3_factors_2_3, fit.measures=TRUE, standardized=TRUE)
 interpret(fit_3_factors_2_3)
@@ -191,8 +206,9 @@ lavaanPlot(model = fit_3_factors_2_3, sig=.05,stars=c("regress","latent","covs")
 cfa_study_2_3 <- 'g_study2 =~ Auditory + Visual + Pain + Tactile
                   g_study3 =~ Auditory_study3 + Visual_study3'
 fit_study_2_3 <- cfa(model = cfa_study_2_3, 
-                     data = all_dataset_Mratio, 
-                     missing="pairwise")
+                     estimator = 'MLR',
+                     data = data2and3, 
+                     missing="FIML")
 
 summary(fit_study_2_3, fit.measures=TRUE, standardized=TRUE)
 interpret(fit_study_2_3)
@@ -205,7 +221,6 @@ lavaanPlot(model = fit_study_2_3, sig=.05,stars=c("regress","latent","covs"),
 
 # AIC comparision
 aictab(cand.set = list(fit_full_2_3, fit_3_factors_2_3, fit_study_2_3), modnames = c('full generality', 'per modality', 'per study'))
-aictab(cand.set = list(fit_full_2_3, fit_study_2_3), modnames = c('full generality', 'per study'))
 
 
 ## Exploratory structural equation modeling --------------------------------------------
