cc3a252b0826570f5fe974d998395ae2ae843878
Audrey Mazancieux <audrey.mazancieux@gmail.com>
2025-03-25 15:10:00 +0100

    Delete SEM analyses

--- analyses_cfa_sem.R
+++ analyses_cfa_sem.R
@@ -74,38 +74,21 @@ fit_4_factors <- cfa(model = cfa_4_factors,
                      data=all_dataset_Mratio, 
                      missing="FIML")
 
-summary(fit_4_factors, fit.measures=TRUE, standardized=TRUE)
-interpret(fit_4_factors)
-
-
-lavaanPlot(model = fit_4_factors, sig=.05,stars=c("regress","latent","covs"),
-           graph_options = list(rankdir = "LR", layout = "circo"),
-           node_options = list(shape = "box", fontname = "Arial"),
-           edge_options = list(color = "grey"),
-           coefs = TRUE, covs = TRUE, stand=TRUE)
-
-
 # study model
 # --> NOT IDENTIFIED
 cfa_3_studies <- 'study_1 =~ VP + EM + SM + EF
                   study_2 =~ Auditory + Visual + Pain + Tactile
                   study_3 =~ Visual_study3 + Auditory_study3'
 fit_3_studies <- cfa(model = cfa_3_studies, 
-                     estimator = 'MLR',
                      data=all_dataset_Mratio, 
+                     estimator = 'MLR',
                      missing="FIML")
 
-summary(fit_3_studies, fit.measures=TRUE, standardized=TRUE)
-interpret(fit_3_studies)
-
-lavaanPlot(model = fit_3_studies, sig=.05,stars=c("regress","latent","covs"),
-           node_options = list(shape = "box", fontname = "Arial"),
-           edge_options = list(color = "grey"),
-           coefs = TRUE, covs = TRUE, stand=TRUE)
-
-
-# AIC comparision
+# AIC comparision for idenitified models
 aictab(cand.set = list(fit_full_general, fit_2_factors), modnames = c('full generality', '2 factors'))
+
+# likelihood ratio test for nested models
+lavTestLRT(fit_full_general, fit_2_factors)
 
 
 ## Confirmatory factor analyses: Study 1 and 3 -----------------------------
@@ -163,6 +146,10 @@ lavaanPlot(model = fit_study_1_3, sig=.05,stars=c("regress","latent","covs"),
 
 # AIC comparision
 aictab(cand.set = list(fit_full_1_3, fit_2_factors_1_3, fit_study_1_3), modnames = c('full generality', 'per modality', 'per study'))
+
+# likelihood ratio test for nested models
+lavTestLRT(fit_full_1_3, fit_2_factors_1_3)
+lavTestLRT(fit_full_1_3, fit_study_1_3)
 
 
 ## Confirmatory factor analyses: Study 2 and 3 -----------------------------
@@ -222,112 +209,7 @@ lavaanPlot(model = fit_study_2_3, sig=.05,stars=c("regress","latent","covs"),
 # AIC comparision
 aictab(cand.set = list(fit_full_2_3, fit_3_factors_2_3, fit_study_2_3), modnames = c('full generality', 'per modality', 'per study'))
 
-
-## Exploratory structural equation modeling --------------------------------------------
-
-data_for_sem <- merge(all_dataset_Mratio, all_dataset_bias, by="Pp")
-
-# Define models for all datatsets
-
-sem1.1 <- '
-# measurement model
-g_metacog_bias =~ Bias_auditory + Bias_auditory_study3 + Bias_visual + Bias_visual_study3 + Bias_pain + Bias_tactile + Bias_EM + Bias_VP + Bias_SM + Bias_EF 
-Perceptual_metacog_eff =~ Auditory + Auditory_study3 + Visual + Visual_study3 + Pain + Tactile + VP
-Cognitive_metacog_eff =~ EM + SM + EF 
-# regressions
-Perceptual_metacog_eff ~ g_metacog_bias
-Cognitive_metacog_eff ~ g_metacog_bias'
-
-sem1.2 <- '
-# measurement model
-Perceptual_metacog_bias =~ Bias_auditory + Bias_auditory_study3 + Bias_visual + Bias_visual_study3 + Bias_pain + Bias_tactile + Bias_VP
-Cognitive_metacog_bias =~ Bias_EM + Bias_SM + Bias_EF
-Perceptual_metacog_eff =~ Auditory + Auditory_study3 + Visual + Visual_study3 + Pain + Tactile + VP
-Cognitive_metacog_eff =~  EM + SM + EF 
-# regressions
-Perceptual_metacog_eff ~ Perceptual_metacog_bias
-Cognitive_metacog_eff ~ Cognitive_metacog_bias'
-
-
-# Fit models
-
-fit_sem1.1 <- sem(sem1.1, data=data_for_sem, missing="FIML")
-fit_sem1.2 <- sem(sem1.2, data=data_for_sem, missing="FIML")
-
-interpret(fit_sem1.1)
-interpret(fit_sem1.2)
-
-summary(fit_sem1.1, fit.measures=TRUE, standardized=TRUE)
-summary(fit_sem1.2, fit.measures=TRUE, standardized=TRUE)
-
-
-# AIC comparision
-aictab(cand.set = list(fit_sem1.1, fit_sem1.2), modnames = c('perception and cognitive efficiency', 'perception and cognitive efficiency and bias'))
-
-
-lavaanPlot(model = fit_sem1.2, sig=.05,stars=c("regress","latent","covs"),
-           node_options = list(shape = "box", fontname = "Arial"),
-           edge_options = list(color = "grey"),
-           coefs = TRUE, covs = TRUE, stand=TRUE)
-
-lavaanPlot(model = fit_sem1.1, sig=.05,stars=c("regress","latent","covs"),
-           node_options = list(shape = "box", fontname = "Arial"),
-           edge_options = list(color = "grey"),
-           coefs = TRUE, covs = TRUE, stand=TRUE)
-
-
-# Define models for Study 1-3
-
-sem2.1 <- '
-# measurement model
-g_metacog_bias =~ Bias_auditory_study3 + Bias_visual_study3 + Bias_EM + Bias_VP + Bias_SM + Bias_EF 
-Perceptual_metacog_eff =~ Auditory_study3 + Visual_study3 + VP
-Cognitive_metacog_eff =~ EM + SM + EF 
-# regressions
-Perceptual_metacog_eff ~ g_metacog_bias
-Cognitive_metacog_eff ~ g_metacog_bias'
-
-sem2.2 <- '
-# measurement model
-Perceptual_metacog_bias =~ Bias_auditory_study3 + Bias_visual_study3 + Bias_VP
-Cognitive_metacog_bias =~ Bias_EM + Bias_SM + Bias_EF
-Perceptual_metacog_eff =~ Auditory_study3 + Visual_study3 + VP
-Cognitive_metacog_eff =~  EM + SM + EF 
-# regressions
-Perceptual_metacog_eff ~ Perceptual_metacog_bias
-Cognitive_metacog_eff ~ Cognitive_metacog_bias'
-
-# Fit models
-
-fit_sem2.1 <- sem(sem2.1, data=data_for_sem, missing="FIML")
-fit_sem2.2 <- sem(sem2.2, data=data_for_sem, missing="FIML")
-
-interpret(fit_sem2.1)
-interpret(fit_sem2.2)
-
-summary(fit_sem2.1, fit.measures=TRUE, standardized=TRUE)
-summary(fit_sem2.2, fit.measures=TRUE, standardized=TRUE)
-
-# AIC comparision
-aictab(cand.set = list(fit_sem2.1, fit_sem2.2), modnames = c('perception and cognitive efficiency', 'perception and cognitive efficiency and bias'))
-
-
-# Define models for Study 2-3
-
-sem3 <- '
-# measurement model
-g_metacog_bias =~ Bias_auditory + Bias_auditory_study3 + Bias_visual + Bias_visual_study3 + Bias_pain + Bias_tactile 
-g_metacog_eff =~ Auditory + Auditory_study3 + Visual + Visual_study3 + Pain + Tactile
-# regressions
-g_metacog_eff ~ g_metacog_bias'
-
-
-# Fit models
-
-fit_sem3 <- sem(sem3, data=data_for_sem, missing="FIML")
-interpret(fit_sem3)
-summary(fit_sem3, fit.measures=TRUE, standardized=TRUE)
-
-
-
+# likelihood ratio test for nested models
+lavTestLRT(fit_full_2_3, fit_study_2_3)
+lavTestLRT(fit_full_2_3, fit_3_factors_2_3)
 
